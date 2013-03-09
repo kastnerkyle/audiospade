cff192f478c50ce9faef914426585d73a4bb9a05
Kyle Kastner <kastnerkyle@gmail.com>
2013-03-09 07:23:36 +0000

    Added more touches to autosetup script

--- ec2_bootstrap.sh
+++ ec2_bootstrap.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Make sure to set deploy, ubuntu, and root account passwords!"
-echo "Also copy the contents of id_rsa.pub from your local machine into the file /home/deploy/.ssh/authorized_keys"
-echo "Set user permissions for sudo appropriately in visudo"
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install fail2ban
@@ -20,5 +17,8 @@ sudo apt-get install python-dev python-virtualenv libpq-dev postgresql python-pi
 virtualenv venv
 cd venv
 git clone git://github.com/esbullington/flask-bootstrap.git
-bin/pip install -r flask-boostrap/requirements.txt
-
+bin/pip install -r flask-bootstrap/requirements.txt
+echo "Make sure to set deploy, ubuntu, and root account passwords!"
+echo "Also copy the contents of id_rsa.pub from your local machine into the file /home/deploy/.ssh/authorized_keys"
+echo "Set user permissions for sudo appropriately in visudo"
+echo "We will also need to setup the psql database by hand for now"
