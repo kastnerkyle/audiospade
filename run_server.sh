38aa999c7be507aa2480f6aaa03633bd58b69c03
Kyle Kastner <kastnerkyle@gmail.com>
2013-03-27 22:24:05 +0000

    Fixed server with nohup (turns out while loops that repeat infinitely at computer speed are bad when things crash)

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
 while [ 1 ]; do
-    nohup sudo venv/bin/python flask-bootstrap/app.py
+    nohup sudo venv/bin/python flask-bootstrap/app.py 2>&1 >> nohup.out
+    sleep 5
 done
